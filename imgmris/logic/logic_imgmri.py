098d08f71f77919e6ef4aa19c4264d9943134aa7
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-03-13 20:13:25 -0500

    a ver si sirve

--- imgmris/logic/logic_imgmri.py
+++ imgmris/logic/logic_imgmri.py
@@ -1,4 +1,5 @@
 from ..models import ImgMri
+from examenes.models import Examen
 
 def get_imgmris():
     queryset = ImgMri.objects.all()
@@ -7,4 +8,25 @@ def get_imgmris():
 def create_imgmris(form):
     measurement = form.save()
     measurement.save()
+    return ()
+
+def cargarPrueba():
+    EXAMENES = {
+    "sub-00001_T1": "T1",
+    "sub-00001_T2": "T2",
+    "sub-00001_T3": "T3"
+    }
+    BASE_GITHUB_URL = "https://raw.githubusercontent.com/sofiaghost/MRI-Archivos/main/examen_1/"
+
+    """Crea registros en la BD con URLs de imágenes en GitHub."""
+    imagenes=ImgMri.objects.all()
+    if len(imagenes)==0:
+        examen = Examen.objects.get_or_create(solicitud=1, paciente="sub-00001", cc=123456, fecha="2021-01-01")
+
+        for folder in EXAMENES.items():
+
+            for i in range(100):  # Suponiendo que cada folder tiene 100 imágenes
+                image_url = f"{BASE_GITHUB_URL}{folder}/slice_{i:03d}.png"
+                # Intentar guardar la imagen en la base de datos
+                ImgMri.objects.create(examen=examen, url=image_url)
     return ()
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from imgmris.logic.logic_imgmri import cargarPrueba
 
 def index(request):
+    cargarPrueba()
     return render(request, 'index.html')
