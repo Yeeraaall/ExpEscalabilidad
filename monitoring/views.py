4a273ab76ea6ccdc95f770199515773daec5758c
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-04-03 17:07:23 -0500

    historia de inicio

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..historiaClinica.models import HistoriaClinica
 
 def index(request):
+    a=HistoriaClinica.objects.all()
+    if len(a) == 0:
+        HistoriaClinica.objects.create(id=1, paciente='Juan', cc=123456)
     return render(request, 'index.html')
 
 def healthCheck(request):
