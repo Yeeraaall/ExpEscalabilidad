977a7d4cf4b59cec2758538f20d46ac460c06e47
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-04-03 20:29:06 -0500

    arreglitos

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -20,7 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('', include('eventoMedico.urls')),
-    path('', include('historiaClinica.urls')),
+    path('', include('eventos.urls')),
+    path('', include('historias.urls')),
     path('health-check/', views.healthCheck)
 ]
